3f36aff7dfaa1de5c092b612833f11596897c89c
Noah Clarkson <unprotesting.email@gmail.com>
2023-11-19 19:06:20 +1300

    Add basic functionality and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use email_newsletter_example::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
